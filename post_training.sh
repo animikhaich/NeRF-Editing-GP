36f767a016ad950a03ddb5ea6c446564d64c4f0d
Animikh Aich <animikhaich@gmail.com>
2023-05-12 19:12:33 -0400

    Added Documentation and comments

--- post_training.sh
+++ post_training.sh
@@ -4,11 +4,11 @@ export LD_LIBRARY_PATH=''
 
 cd /home/ani/Projects/gp/NeRF-Editing/src
 
-# Extract Mesh
+# Extract Mesh (from NeRF)
 echo "[LOG] Extracting Mesh"
 python exp_runner.py --mode validate_mesh --conf ./confs/womask_hbychair.conf --case hbychair_neus --is_continue
 
-# Render before deformation
+# Render before deformation (for comparison)
 echo "[LOG] Rendering before deformation"
 python exp_runner.py --mode circle --conf ./confs/womask_hbychair_render.conf --case hbychair_neus --is_continue  --obj_path ./logs/hbychair_wo_mask/meshes/00170000.obj
 ffmpeg -i ./logs/hbychair_wo_mask/render_circle/video.mp4 ./logs/hbychair_wo_mask/render_circle/out_1.mp4 -y
@@ -17,12 +17,12 @@ ffmpeg -i ./logs/hbychair_wo_mask/render_circle/video.mp4 ./logs/hbychair_wo_mas
 echo "[LOG] Doing Dialation (Construct Cage Mesh)"
 python exp_runner.py --mode validate_mesh --conf ./confs/womask_hbychair.conf --case hbychair_neus --is_continue --do_dilation
 
-# TetWild
+# TetWild (Construct Tetrahedral Mesh)
 echo "[LOG] TetWild"
 cd /home/ani/Projects/gp/NeRF-Editing/TetWild/build
 ./TetWild ../../src/logs/hbychair_wo_mask/meshes/00170000.obj
 
-# Create OVM
+# Create OVM (Construct OpenVolumeMesh)
 echo "[LOG] Create OVM"
 cd /home/ani/Projects/gp/NeRF-Editing/OpenVolumeMesh/build
 ./simple_mesh ../../src/logs/hbychair_wo_mask/meshes/00170000_.txt ../../src/logs/hbychair_wo_mask/mesh_cage_nofloor_.ovm
--- render_deform.sh
+++ render_deform.sh
@@ -2,17 +2,17 @@
 
 export LD_LIBRARY_PATH=''
 
-# Barysentric
+# Barysentric Control Points
 echo "[LOG] Barysentric"
 cd /home/ani/Projects/gp/NeRF-Editing/src
 python barycentric_control_pts_jittor.py
 
-# Volume ARAP
+# Volume ARAP (Deformation)
 echo "[LOG] Volume ARAP"
 cd /home/ani/Projects/gp/NeRF-Editing/volumeARAP_batch/build
 ./volumeARAP ../../src/logs/hbychair_wo_mask/mesh_cage_nofloor_.ovm ../../src/logs/hbychair_wo_mask/mesh_seq/2_barycentric_control.txt ../../src/logs/hbychair_wo_mask/mesh_seq_ovm 0
 
-# Render after deformation
+# Render after deformation (for comparison)
 cd /home/ani/Projects/gp/NeRF-Editing/src
 python exp_runner.py --mode circle --conf ./confs/womask_hbychair_render.conf --case hbychair_neus --is_continue --use_deform --reconstructed_mesh_file ./logs/hbychair_wo_mask/meshes/00170000_.txt --deformed_mesh_file ./logs/hbychair_wo_mask/mesh_seq_ovm/arap_result_0000_.ovm --obj_path ./logs/hbychair_wo_mask/mesh_seq/2.obj && \
 ffmpeg -i ./logs/hbychair_wo_mask/render_circle/video.mp4 ./logs/hbychair_wo_mask/render_circle/out_2.mp4 -y
